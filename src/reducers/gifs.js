00fb0a0677362c666eeb75b3a01ef6e73974d3c4
gary <ggrumbley@ucsd.edu>
2017-01-20 14:55:25 -0800

    Favorites page added

--- src/reducers/gifs.js
+++ src/reducers/gifs.js
@@ -1,13 +1,20 @@
 const initialState = {
-  data: []
+  data: [],
+  favorites: []
 };
 
 export default function gifs(state = initialState, action) {
   switch (action.type) {
     case 'REQUEST_GIFS':
-      return {
-        ...state, data: action.payload
+      return { ...state, data: action.payload }
+    case 'FETCH_FAVORITED_GIFS':
+      let arr = []
+      for (let i in action.payload) {
+        if (action.payload.hasOwnProperty(i)) {
+          arr.push(action.payload[i])
+        }
       }
+      return { ...state, favorites: arr }
     default:
       return state;
   }
